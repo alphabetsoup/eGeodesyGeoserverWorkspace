a32c7fed72b0ae51d34f49dd4f1249bb0128eba7
Laurence Davies <lsdavies@mailc.net>
2015-05-21 09:16:13 +0100

    Adding all types

--- dbmigrate/views/view.sql
+++ dbmigrate/views/view.sql
@@ -1,4 +1,5 @@
 drop view sites_simple;
+drop view sitelogs_simple;
 drop view positions_simple;
 drop view nodes_simple;
 drop view adjustments_simple;
@@ -6,6 +7,7 @@ drop view adjustments_simple;
 create view sites_simple as
 (
     select concat('site_',md.mark_id) as gid, 
+    md.mark_id as site_id, 
     md.mark_id, 
     mn.nine_fig, mn.mark_name,
     ms.status_txt, md.used_date,
@@ -19,17 +21,31 @@ create view sites_simple as
     join name_type as nt on mn.name_type = nt.name_type
 );
 
+create view sitelogs_simple as
+(
+    select concat('sitelog_',md.mark_id) as gid, 
+    ST_SETSRID(ST_POINT(mc.longitude, mc.latitude), datum.d_epsg_code) as srs_loc
+    from mark_description as md
+    join mark_coordinates as mc on mc.mark_id = md.mark_id
+    join datum on datum.d_code = mc.datum_code
+);
+
 create view nodes_simple as
 (
     select concat('node_',md.mark_id) as gid, 
+    md.mark_id as at_site, 
+    md.mark_id as node_id, 
     md.mark_id, 
     ms.status_txt, md.date_mga_avail,
     mc.organisation,
     mc.date_surv, mc.date_edit,
+    at.adj_type_txt,
     ST_SETSRID(ST_POINT(mc.longitude, mc.latitude), datum.d_epsg_code) as srs_loc
     from mark_description as md
     join mark_name as mn on mn.mark_id = md.mark_id
     join mark_coordinates as mc on mc.mark_id = md.mark_id
+    join adjustment as a on a.adj_id = mc.adj_id
+    join adjustment_type as at on a.adj_type = at.adj_type
     join datum on datum.d_code = mc.datum_code
     join mark_status as ms on ms.status = md.status
     join name_type as nt on mn.name_type = nt.name_type
@@ -38,6 +54,7 @@ create view nodes_simple as
 create view positions_simple as
 (
     select concat('position_',mc.mark_coord_id) as gid, 
+    mc.mark_id as at_node, 
     md.mark_id, 
     ms.status_txt, md.date_mga_avail,
     mc.date_surv, mc.date_edit,
@@ -56,6 +73,7 @@ create view positions_simple as
     mc.best_coords,
     mc.ellipsoid_height,
     mc.pos_uncertainty,
+    datum.d_epsg_code,
 
     ST_SETSRID(ST_POINT(mc.longitude, mc.latitude), datum.d_epsg_code) as srs_loc
 
@@ -89,6 +107,7 @@ create view adjustments_simple as
     a.confidence_interval,
     a.adj_type, 
     at.adj_type_txt,
+    datum.d_epsg_code,
 
     ST_SETSRID( ST_Extent(ST_POINT(mc.longitude,mc.latitude)), datum.d_epsg_code) as srs_envelope
 
